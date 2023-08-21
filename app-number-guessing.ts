2c7ac0190659a1e292b1984e884a8065ee741ec5
Taha khan <tahak6715@gmail.com>
2023-08-21 09:55:15 -0700

    work complete

---
+++ app-number-guessing.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let answers = await inquirer.prompt([{
+    name: "guess",
+    type: "number",
+    message: "Enter the Guessed Number:"}
+]);
+
+
+
+
+let num : number = Math.random()*10;
+let num1 = Math.floor(num);
+
+console.log(chalk.bold(chalk.blue("Welcome To Number Guessing Game")));
+console.log(chalk.red("Hint number will be Between 1 to 10"));
+
+if (answers.guess === num1){
+    console.log(chalk.bgBlue("Congats!! You Guessed the Correct Number.."))
+}
+else{
+    console.log(chalk.bgBlue("Try Again.."))
+}
+
+
